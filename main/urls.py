c4943626898f8340a5953846fe8f8ff266ed3e86
nurulloh <nurullomahmud@gmail.com>
2023-10-28 15:19:57 +0500

    nav changed

--- main/urls.py
+++ main/urls.py
@@ -24,7 +24,7 @@ from .views import (
 
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
-    path('home', BaseView.as_view(), name='home'),
+    path('home/', BaseView.as_view(), name='home'),
     path('used/', UsedView.as_view(), name='used'),
     path('available/', AvailableCodesView.as_view(), name='available'),
     path('form/<int:amount>', Form.as_view(), name='form'),
@@ -32,12 +32,12 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('paid/<str:money_code>', PaidStatusView.as_view(), name='paid'),
     path('voided/<str:money_code>', VoidedStatusView.as_view(), name='voided'),
-    path('staff', StaffView.as_view(), name='staff'),
+    path('staff/', StaffView.as_view(), name='staff'),
     path('activate/<int:pk>', ActivateStaffView.as_view(), name='activate'),
     path('deactivate/<int:pk>', DeactivateStaffView.as_view(), name='deactivate'),
-    path('add_message', AddMessageView.as_view(), name='add_message'),
+    path('add_message/', AddMessageView.as_view(), name='add_message'),
     path('edit/<str:money_code>', EditNoteView.as_view(), name='edit_note'),
     path('not-found', PageNotFoundView.as_view(), name='not-found'),
     path('add-staff', AddStaff.as_view(), name='add-staff'),
-    path('archive', ArchiveView.as_view(), name='archive'),
+    path('archive/', ArchiveView.as_view(), name='archive'),
 ]
